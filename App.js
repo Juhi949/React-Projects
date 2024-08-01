fa175038706a435b8f4bc33fe1b20fd8c3dd0bbe
Juhi949 <64358615+Juhi949@users.noreply.github.com>
2024-08-01 17:21:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Intro from './components/Intro/Intro';
+import Navbar from './components/Navbar/Navbar';
+import Skills from './components/Skills/Skills';
+import Works from './components/Works/Works';
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Intro/>
+      <Skills/>
+      <Works/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
